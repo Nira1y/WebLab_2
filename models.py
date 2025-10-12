5eed6b7a96e3e33ae6a51445c08f38d6b0bd14d0
yanl1n <yanl1n.wrk@mail.ru>
2025-10-12 21:16:14 +1000

    added user and article models

---
+++ models.py
@@ -0,0 +1,36 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash 
+
+data_base = SQLAlchemy()
+
+class User(UserMixin, data_base.Model):
+    __tablename__= "user"
+
+    id = data_base.Column(data_base.Integer, primary_key = True)
+    name = data_base.Column(data_base.String(100), nullable=False)
+    email = data_base.Column(data_base.String(100), nullable=False)
+    hashed_password = data_base.Column(data_base.String(200), nullable=False)
+    create_date = data_base.Column(data_base.DateTime, default=datetime.datezone.utc)
+    articles = data_base.relationship('Article', backref='author', lazy=True)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
+class Article(data_base.Model):
+    __tablename__= "articles"
+
+    id = data_base.Column(data_base.Integer, primary_key = True)
+    title = data_base.Column(data_base.String(200), nullable=False)
+    text = data_base.Column(data_base.Text, nullable=False)
+    create_date = data_base.Column(data_base.DateTime, default=datetime.datezone.utc)
+    user_id = data_base.Column(data_base.Integer, data_base.ForeignKey('user.id'), nullable=False)
+    category = data_base.Column(data_base.String(50), default='general')
+    comment = data_base.relation('Comment', backref='article', lazy='dynamic')
+
+class Comment(data_base.Model):
+    None
